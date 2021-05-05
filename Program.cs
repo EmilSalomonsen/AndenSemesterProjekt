122046fe06b32c3b642976b7820b482adda414fb
EmilSalomonsen <emil26q4@edu.easj.dk>
2021-05-05 12:44:38 +0200

    gitchampen ham selv

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace AndenSemesterProjekt
             CreateHostBuilder(args).Build().Run();
         }
 
+        // hva så gitchamp
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
