8218f3ba54f2b921148789c7a5f7bf417afd5efc
ThunderBlaZe778 <anderslring@yahoo.dk>
2021-05-10 09:58:19 +0200

    Puttede userstory 3 ind i attendants og drivers

--- Models/Attendants.cs
+++ Models/Attendants.cs
@@ -7,6 +7,15 @@ namespace AndenSemesterProjekt
 {
     public class Attendants : Users
     {
-        public bool IAmAttendant;
+        public bool IAmAttendant { get; set; }
+
+
+        /*this method takes an attendant and grabs all the data from said attendant and creates a new driver using the data from the attendant,
+        then deletes the attendant from the attendant database, so the user only shows up in one of the two database tables*/
+        public void changeToDriver(Attendants Attendant)
+        {
+            addDriver(Attendant.getNavn(), Attendant.getPassword(), Attendant.getAdresse());
+            Attendants.sletAttendant(Attendant.getID());
+        }
     }
 }
