beb45886321e5c7492965b1538fcd31c726f5261
EmilSalomonsen <emil26q4@edu.easj.dk>
2021-05-24 00:50:35 +0200

    Tilføjet SQL script til create database og tilføjet assignattendant model

---
+++ Models/AssignAttendant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AndenSemesterProjekt.Models
+{
+    public class AssignAttendant
+    {
+        [Key]
+        [Column("AssignAttendantID")]
+        public int AssignAttendantId { get; set; }
+
+        [ForeignKey("RouteID")]
+        [InverseProperty("AssignAttendant")]
+        public virtual Route Route { get; set; }
+
+        [ForeignKey("AttendantID")]
+        [InverseProperty("AssignAttendant")]
+        public virtual Attendant Attendant { get; set; }
+    }
+}
